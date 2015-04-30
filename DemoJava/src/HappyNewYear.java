7d8788e98cbb1231b7323359a1aea5b7d34045ef
vibhatha <vibhatha@gmail.com>
2015-04-30 11:47:09 -0700

    My first commit via Eclipse

---
+++ DemoJava/src/HappyNewYear.java
@@ -0,0 +1,10 @@
+
+public class HappyNewYear {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Happy New Year...");
+		System.out.println("සුබ නව වසරක් වේවා ");
+	}
+
+}
